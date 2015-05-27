9f6a556f13239706921ad29a89db5db58e40dc1a
Pral2a <guillem@pral2a.com>
2015-05-27 15:26:17 +0200

    Phant storage

--- index.js
+++ index.js
@@ -2,38 +2,30 @@ var request = require('request');
 var cheerio = require('cheerio');
 var colors = require('colors');
 var express = require('express');
+var Phant = require('phant-client').Phant;
 var config = require('./config.json');
 
 
 var app = express();
+var phant = new Phant();
 
 var http = require('http').Server(app);
 
-var auth = "Basic " + new Buffer(config.wlc.username + ":" + config.wlc.password).toString("base64");
-
-var url =  config.wlc.end_point_url + '/screens/base/monitor_summary.html';
-
 app.get('/wifi', function (req, res) {
-  request.get({
-      url : url,
-      headers : {
-          "Authorization" : auth
-      }
-    }, 
-    function(error, response, body) { 
-      var t = cheerio.load(body);
-      var r = {
-        clients: t('input[name="1.0.1.wlan_no_of_clients"]').val(),
-        ap: {
-          up: t('input[name="current_up_aps"]').val(),
-          down: t('input[name="current_down_aps"]').val()
-        }
-      }
-
-      res.json(r);
-    }
-  );
+  getWLCdata(config.wlc, function(data){
+    res.json(data);
+  });
 });
+
+getWLCdata(config.wlc, function(data){
+  phant.add(config.store.phant, { clients: data.clients });
+});
+
+setInterval(function() {
+  getWLCdata(config.wlc, function(data){
+    phant.add(config.store.phant, { clients: data.clients });
+  });
+}, config.store.query_time);
 
 http.listen(4000, function () {
     console.log(colors.rainbow("Listening..."));
@@ -43,3 +35,25 @@ process.on('exit', function(code) {
     http.close();
     console.log(colors.rainbow("Quitting..."));
 });
+
+
+function getWLCdata (wlc, callback) {
+  request.get({
+      url : config.wlc.end_point_url + '/screens/base/monitor_summary.html',
+      headers : {
+          "Authorization" : "Basic " + new Buffer(config.wlc.username + ":" + config.wlc.password).toString("base64")
+      }
+    }, 
+    function(error, response, body) { 
+      var t = cheerio.load(body);
+      var r = {
+        clients: Number(t('input[name="1.0.1.wlan_no_of_clients"]').val()),
+        ap: {
+          up: Number(t('input[name="current_up_aps"]').val()),
+          down: Number(t('input[name="current_down_aps"]').val())
+        }
+      }
+      callback(r);
+    }
+  );
+}
